44cf9744581575b8f89e78f864c1974446b30809
Tom J <thomasjdjoyce@gmail.com>
2023-11-28 14:56:52 +0000

    Interface added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main extends VehicleHiringTest{
+    public static void main(String[] args) {
+
+        testAddToMap(String reg, Location loc);
+        testMoveVehicle(String reg, Location loc);
+        testRemoveVehicle(String reg);
+        testGetVehicleLoc(String reg);
+        testGetVehiclesInRange(Location loc, int r);
+    }
+}
